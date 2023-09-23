bdc702df99949eb126cd7fc3ded158d5e2673acb
Zen Lucas <2200501@slu.edu.ph>
2023-09-23 22:33:15 +0800

    testing out php

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "WHAT'S UP MGA KAIBIGAN<br>\n";
+        echo "Date is: " . date('j-m-y, h:i:s');
+    ?>
+</body>
+</html>
